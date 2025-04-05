feefda137be552839cf3d572db1e4e82b190e275
hecate cantus <sys@hecate.space>
2025-04-04 22:22:16 -0700

    password-protect MPD, template some more stuff
    this is load-bearing shitty code so everything else stays pretty

--- eggs/scripts/gen_yolk_secrets_files.sh
+++ eggs/scripts/gen_yolk_secrets_files.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 BW_SESSION=$(bw login --raw || bw unlock --raw)
 
+bw sync
+
 # future: for-loop the domains & outputs
 mkdir -p ~/.config/yolk/secrets
 
 bw get password last.fm > ~/.config/yolk/secrets/last.fm
+bw get password mpd     > ~/.config/yolk/secrets/mpd
--- eggs/scripts/now_playing.sh
+++ eggs/scripts/now_playing.sh
@@ -7,7 +7,7 @@ tput clear
 
 function print_centered {
 	# trust me bro you do not need to comprehend the horrors
-	str_len=${#1}
+	str_len=${#1}  #{< rr("XD", "#1") >}
 	spaze=$(( ($COLUMNS - $str_len)/2 ))
 	left_padding=$((spaze + str_len))	
 	left_padded=${(l:left_padding:)1}
@@ -65,5 +65,5 @@ while true; do
 	mpc current --wait 2>&1 >/dev/null & wait_pid=$!
 	wait
 	wait_pid=
-	# ILL FUCKEN DO IT AGAIN
+	# DO IT AGAIN!
 done
