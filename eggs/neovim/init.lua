eabacea012814720447da0b60213a1e3fcb52ed4
hecate cantus <sys@hecate.space>
2025-07-04 15:14:42 -0700

    set up tmux; general update from macbook air
    BRACE YOURSELF MERGE COMMITS ARE COMING

--- eggs/neovim/init.lua
+++ eggs/neovim/init.lua
@@ -1,15 +1,5 @@
 require("config.lazy")
 
----Plug('rhaiscript/vim-rhai')
----Plug('imsnif/kdl.vim')
----Plug('vim-airline/vim-airline')
----Plug('vim-airline/vim-airline-themes')
-
----vim.opt_global.airline_theme = 'molokai'
----vim.opt_global.airline_powerline_fonts = 1
----vim.opt_global.airline#extensions#tabline#enabled = 1
----vim.opt_global.airline#extensions#whitespace#mixed_indent_algo = 1
-
 vim.cmd("nnoremap <Tab> :bnext<CR>")
 vim.cmd("nnoremap <S-Tab> :bprevious<CR>")
 -- TODO keybind to toggle normal to below bg/fg and back
@@ -22,3 +12,7 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.title = true
 vim.opt.wrap = false
+
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
